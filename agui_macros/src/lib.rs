51fdca3a3d62db79e772dd59a7408c4316ae44c0
Stumblinbear <stumblinbear@gmail.com>
2021-12-25 17:48:58 -0600

    Update lib.rs

--- agui_macros/src/lib.rs
+++ agui_macros/src/lib.rs
@@ -1,19 +1,19 @@
 extern crate proc_macro;
 
-use functional_widget::parse_functional_widget;
+// use functional_widget::parse_functional_widget;
 use proc_macro::TokenStream;
 use proc_macro_error::{abort_call_site, proc_macro_error};
 use widget_derive::parse_widget_derive;
 
 mod builder;
-mod functional_widget;
+// mod functional_widget;
 mod layout;
 mod widget_derive;
 
-#[proc_macro_attribute]
-pub fn functional_widget(args: TokenStream, input: TokenStream) -> TokenStream {
-    parse_functional_widget(args, input)
-}
+// #[proc_macro_attribute]
+// pub fn functional_widget(args: TokenStream, input: TokenStream) -> TokenStream {
+//     parse_functional_widget(args, input)
+// }
 
 #[proc_macro_derive(Widget, attributes(widget))]
 pub fn widget_derive(input: TokenStream) -> TokenStream {
