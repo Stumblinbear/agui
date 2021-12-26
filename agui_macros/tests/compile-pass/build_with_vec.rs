bb639029df3a368c1e4e7dacc8ae2ad63601b1db
Stumblinbear <stumblinbear@gmail.com>
2021-12-25 18:08:54 -0600

    fix compile tests

--- agui_macros/tests/compile-pass/build_with_vec.rs
+++ agui_macros/tests/compile-pass/build_with_vec.rs
@@ -1,6 +1,6 @@
-use agui_core::{unit::Color, widget::WidgetRef};
+use agui_core::widget::WidgetRef;
 use agui_macros::build;
-use agui_primitives::Quad;
+use agui_primitives::{Column, Quad};
 
 fn main() {
     let _widget: WidgetRef = build! {
