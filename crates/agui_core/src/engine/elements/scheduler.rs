a170ca7859005e3b0ead4e675283d2f841e02a6f
Trevin Miller <stumblinbear@gmail.com>
2024-01-30 12:41:10 -0600

    fix: add create element task struct

--- crates/agui_core/src/engine/elements/scheduler.rs
+++ crates/agui_core/src/engine/elements/scheduler.rs
@@ -1,53 +1,98 @@
-use std::{future::Future, mem::MaybeUninit, pin::Pin};
+use std::{
+    future::{Future, IntoFuture},
+    pin::Pin,
+};
 
 use crate::{
-    element::ElementId,
+    element::{ContextElement, ElementId, ElementTaskContext, ElementTaskNotifyStrategy},
     task::{error::TaskError, TaskHandle},
 };
 
-pub type ElementTask = Pin<Box<dyn Future<Output = ()>>>;
+pub struct CreateElementTask {
+    ctx: ElementTaskContext,
+    #[allow(clippy::type_complexity)]
+    func: Box<dyn FnOnce(ElementTaskContext) -> Pin<Box<dyn Future<Output = ()>>>>,
+}
+
+impl CreateElementTask {
+    pub fn element_id(&self) -> ElementId {
+        self.ctx.element_id()
+    }
+
+    pub fn with_notify_strategy<T>(self, strategy: T) -> Self
+    where
+        T: ElementTaskNotifyStrategy + 'static,
+    {
+        Self {
+            ctx: self.ctx.with_notify_strategy(strategy),
+
+            func: self.func,
+        }
+    }
+}
+
+impl IntoFuture for CreateElementTask {
+    type Output = ();
+
+    type IntoFuture = Pin<Box<dyn Future<Output = ()>>>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        (self.func)(self.ctx)
+    }
+}
 
 #[allow(unused_variables)]
 pub trait ElementSchedulerStrategy {
-    fn spawn_task(&self, id: ElementId, task: ElementTask) -> Result<TaskHandle<()>, TaskError>;
+    fn spawn_task(&mut self, task: CreateElementTask) -> Result<TaskHandle<()>, TaskError>;
 }
 
 impl ElementSchedulerStrategy for () {
-    fn spawn_task(&self, _: ElementId, _: ElementTask) -> Result<TaskHandle<()>, TaskError> {
+    fn spawn_task(&mut self, _: CreateElementTask) -> Result<TaskHandle<()>, TaskError> {
         Err(TaskError::no_scheduler())
     }
 }
 
-pub struct ElementScheduler<'ctx, const HAS_STRATEGY: bool = true> {
+pub struct ElementScheduler<'ctx> {
+    strategy: Option<&'ctx mut dyn ElementSchedulerStrategy>,
+
     element_id: &'ctx ElementId,
-    strategy: MaybeUninit<&'ctx mut dyn ElementSchedulerStrategy>,
 }
 
-impl<'ctx> ElementScheduler<'ctx, false> {
+impl<'ctx> ElementScheduler<'ctx> {
     pub(super) fn new(element_id: &'ctx ElementId) -> Self {
         ElementScheduler {
             element_id,
-            strategy: MaybeUninit::uninit(),
+            strategy: None,
         }
     }
 
     pub fn with_strategy<'a>(
         self,
         strategy: &'a mut dyn ElementSchedulerStrategy,
-    ) -> ElementScheduler<'a, true>
+    ) -> ElementScheduler<'a>
     where
         'ctx: 'a,
     {
         ElementScheduler {
             element_id: self.element_id,
-            strategy: MaybeUninit::new(strategy),
+            strategy: Some(strategy),
         }
     }
-}
 
-impl<'ctx> ElementScheduler<'ctx, true> {
-    pub fn spawn_task(&self, task: ElementTask) -> Result<TaskHandle<()>, TaskError> {
-        // SAFETY: `ElementScheduler<true>` is only constructed with a valid strategy
-        unsafe { self.strategy.assume_init_read() }.spawn_task(*self.element_id, task)
+    pub fn spawn_task<Fut>(
+        &mut self,
+        func: impl FnOnce(ElementTaskContext) -> Fut + 'static,
+    ) -> Result<TaskHandle<()>, TaskError>
+    where
+        Fut: Future<Output = ()> + 'static,
+    {
+        let Some(strategy) = self.strategy.as_mut() else {
+            return Err(TaskError::no_scheduler());
+        };
+
+        strategy.spawn_task(CreateElementTask {
+            ctx: ElementTaskContext::new(*self.element_id),
+            func: Box::new(|ctx| Box::pin(func(ctx))),
+        })
     }
 }
