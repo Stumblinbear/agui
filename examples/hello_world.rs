14fb3c17ccb49a442aef6cc54c768c113d907ae5
Trevin Miller <stumblinbear@gmail.com>
2024-02-07 13:22:22 -0600

    fix: basic hello world

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,8 +1,12 @@
+use agui_vello::{
+    binding::VelloViewBinding,
+    renderer::{window::VelloWindowRenderer, VelloRenderer},
+};
+use agui_winit::{WinitWindow, WinitWindowAttributes};
 use tracing::metadata::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
-use agui::{app::run_app, prelude::*, winit::Window};
-use winit::{dpi::PhysicalSize, window::WindowBuilder};
+use agui::{app::run_app, prelude::*};
 
 fn main() {
     let filter = EnvFilter::from_default_env()
@@ -17,16 +21,29 @@ fn main() {
         .with_env_filter(filter)
         .init();
 
-    run_app(build! {
-        <Window> {
-            window: || WindowBuilder::new()
-                .with_title("agui hello world")
-                .with_inner_size(PhysicalSize::new(800.0, 600.0)),
+    let vello_renderer = VelloRenderer::default();
 
-            child: <Text> {
-                style: TextStyle::default().color(Color::from_rgb((1.0, 1.0, 1.0))),
-                text: "Hello, world!".into(),
-            },
+    run_app(move || {
+        let (view, view_handle) = vello_renderer.new_view();
+
+        build! {
+            <WinitWindow> {
+                attributes: WinitWindowAttributes::builder()
+                    .title("Hello, world!")
+                    .build(),
+
+                renderer: VelloWindowRenderer::new(view_handle),
+
+                child: <VelloViewBinding> {
+                    view: view,
+
+                    child: <Text> {
+                        style: TextStyle::default().color(Color::from_rgb((1.0, 1.0, 1.0))),
+                        text: "Hello, world!".into(),
+                    }
+                }
+            }
         }
-    });
+    })
+    .expect("Failed to run app");
 }
